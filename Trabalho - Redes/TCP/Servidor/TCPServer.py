42e33f0bc702c4e3e1dbede5125ca428930d322f
DanCLima <daniel.costa@sou.unifal-mg.edu.br>
2023-07-20 00:43:10 -0300

    Implementar scp no TCP

--- Trabalho - Redes/TCP/Servidor/TCPServer.py
+++ Trabalho - Redes/TCP/Servidor/TCPServer.py
@@ -8,14 +8,32 @@ serverSocket.listen(1)
 
 print("The server is ready to receive")
 
-while 1:
+while True:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode("UTF-8")
+    print("Client connected:", addr)
 
-    if not sentence:
-        break
+    while True:
+        sentence = connectionSocket.recv(1024).decode("UTF-8")
 
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode("UTF-8"))
+        if not sentence:
+            break
 
+        if sentence == "ls":
+            sentence = os.listdir()  # Obtém uma lista de nomes de todos os arquivos e diretórios
+            modifiedSentence = "\n".join(sentence).encode("UTF-8")  # Transforma a lista de nomes de arquivos em uma única string separada por quebra de linhas
+        elif sentence == "pwd":
+            modifiedSentence = os.getcwd().encode("UTF-8")
+        elif sentence.startswith("cd "):
+            caminho = sentence[3:]  # Retira os 3 caracteres iniciais da string
+            try:
+                os.chdir(caminho)  # Acessando o diretório
+                modifiedSentence = ("Diretório alterado para: " + os.getcwd()).encode("UTF-8")
+            except FileNotFoundError:
+                modifiedSentence = "Caminho inválido ou diretório não encontrado.".encode("UTF-8")
+        else:
+            modifiedSentence = "Comando inválido.".encode("UTF-8")
+
+        connectionSocket.send(modifiedSentence)
+
+    print("Client disconnected:", addr)
     connectionSocket.close()
--- Trabalho - Redes/TCP/TCPCliente.py
+++ Trabalho - Redes/TCP/TCPCliente.py
@@ -5,9 +5,21 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-sentence = input("Input lowercase sentence: ")
-clientSocket.send(sentence.encode("UTF-8"))
-modifiedSentence = clientSocket.recv(1024).decode("UTF-8")
+while True:
+    comando = input("Insira um comando: ")
+    if comando == "ls":
+        clientSocket.send(comando.encode("UTF-8"))
+        modifiedSentence = clientSocket.recv(1024).decode("UTF-8")
+        print (modifiedSentence)
+    elif comando == "pwd":
+        clientSocket.send(comando.encode("UTF-8"))
+        modifiedSentence = clientSocket.recv(1024).decode("UTF-8")
+        print (modifiedSentence)
+    elif comando.startswith("cd "):     # Verifica se o início da minha string contém "cd "
+        clientSocket.send(comando.encode("UTF-8"))
+        modifiedSentence = clientSocket.recv(1024).decode("UTF-8")
+        print (modifiedSentence)
+    else:
+        break
 
-print("From Server: ", modifiedSentence)
 clientSocket.close()
