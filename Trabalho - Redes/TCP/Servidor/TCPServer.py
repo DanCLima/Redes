c2abbe8f167dffcc7fd1325e9a9e2a6b0397baf8
DanCLima <daniel.costa@sou.unifal-mg.edu.br>
2023-07-20 21:28:34 -0300

    Quase finalizado

--- Trabalho - Redes/TCP/Servidor/TCPServer.py
+++ Trabalho - Redes/TCP/Servidor/TCPServer.py
@@ -1,5 +1,5 @@
-from socket import *
 import os
+from socket import *
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -8,32 +8,63 @@ serverSocket.listen(1)
 
 print("The server is ready to receive")
 
-while True:
-    connectionSocket, addr = serverSocket.accept()
-    print("Client connected:", addr)
+while 1:
+    connectionSocket, clientAddress = serverSocket.accept()
+    print("Client connected:", clientAddress)
 
-    while True:
-        sentence = connectionSocket.recv(1024).decode("UTF-8")
+    while 1:
+        message = connectionSocket.recv(2048)
+        message = message.decode("UTF-8")
 
-        if not sentence:
+        if not message:
             break
 
-        if sentence == "ls":
-            sentence = os.listdir()  # Obtém uma lista de nomes de todos os arquivos e diretórios
-            modifiedSentence = "\n".join(sentence).encode("UTF-8")  # Transforma a lista de nomes de arquivos em uma única string separada por quebra de linhas
-        elif sentence == "pwd":
-            modifiedSentence = os.getcwd().encode("UTF-8")
-        elif sentence.startswith("cd "):
-            caminho = sentence[3:]  # Retira os 3 caracteres iniciais da string
+        if message == "ls":
+            message = os.listdir()  # Obtém uma lista de nomes de todos os arquivos e diretórios
+            modifiedMessage = "\n".join(message).encode("UTF-8")  # Transforma a lista de nomes de arquivos em uma única string separada por quebra de linhas
+        elif message == "pwd":
+            modifiedMessage = os.getcwd().encode("UTF-8")
+        elif message.startswith("cd "):
+            caminho = message[3:]  # Retira os 3 caracteres iniciais da string
             try:
                 os.chdir(caminho)  # Acessando o diretório
-                modifiedSentence = ("Diretório alterado para: " + os.getcwd()).encode("UTF-8")
+                modifiedMessage = ("Diretório alterado para: " + os.getcwd()).encode("UTF-8")
             except FileNotFoundError:
-                modifiedSentence = "Caminho inválido ou diretório não encontrado.".encode("UTF-8")
+                modifiedMessage = "Caminho inválido ou diretório não encontrado.".encode("UTF-8")
+        elif message.startswith("scp "):
+            nome_arq = message[4:]
+            if os.path.exists(nome_arq):
+                # Enviando o nome do arquivo
+                modifiedMessage = nome_arq.encode("UTF-8")
+                connectionSocket.send(modifiedMessage)
+
+                # Enviando o tamanho do arquivo
+                tam_arquivo = os.path.getsize(nome_arq)
+                modifiedMessage = str(tam_arquivo).encode("UTF-8")  # Transformando em string antes de codificar
+                connectionSocket.send(modifiedMessage)
+
+                # Abre o arquivo como leitura
+                with open(nome_arq, 'rb') as file:
+                    while tam_arquivo > 0:
+                        tam_buffer = 1024 if tam_arquivo >= 1024 else tam_arquivo
+
+                        # Lê os dados do arquivo
+                        dados = file.read(tam_buffer)
+                        tam_arquivo -= len(dados)
+
+                        # Envia os dados para o cliente
+                        connectionSocket.send(dados)
+
+                        # Recebe a confirmação de recebimento do ACK
+                        connectionSocket.recv(1024)
+
+                # Aguarda o próximo comando ou fechamento da conexão
+                continue
+            else:
+                modifiedMessage = "null".encode("UTF-8")
         else:
-            modifiedSentence = "Comando inválido.".encode("UTF-8")
+            break
+        connectionSocket.send(modifiedMessage)
 
-        connectionSocket.send(modifiedSentence)
-
-    print("Client disconnected:", addr)
+    print("Client disconnected:", clientAddress)
     connectionSocket.close()
--- Trabalho - Redes/TCP/TCPCliente.py
+++ Trabalho - Redes/TCP/TCPCliente.py
@@ -1,25 +1,58 @@
+import os
 from socket import *
 
-serverName = 'localhost'
+serverName = 'localhost'  # Usar 'localhost' se estiver na mesma máquina com Windows ou o IP do servidor se estiver em 2 computadores
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
 
-while True:
-    comando = input("Insira um comando: ")
+while 1:
+    comando = input("Insira um comando: \n")
     if comando == "ls":
         clientSocket.send(comando.encode("UTF-8"))
-        modifiedSentence = clientSocket.recv(1024).decode("UTF-8")
-        print (modifiedSentence)
+        modifiedMessage = clientSocket.recv(2048).decode("UTF-8")
+        print(modifiedMessage)
     elif comando == "pwd":
         clientSocket.send(comando.encode("UTF-8"))
-        modifiedSentence = clientSocket.recv(1024).decode("UTF-8")
-        print (modifiedSentence)
-    elif comando.startswith("cd "):     # Verifica se o início da minha string contém "cd "
+        modifiedMessage = clientSocket.recv(2048).decode("UTF-8")
+        print(modifiedMessage)
+    elif comando.startswith("cd "):  # Verifica se o início da minha string contém "cd "
         clientSocket.send(comando.encode("UTF-8"))
-        modifiedSentence = clientSocket.recv(1024).decode("UTF-8")
-        print (modifiedSentence)
+        modifiedMessage = clientSocket.recv(2048).decode("UTF-8")
+        print(modifiedMessage)
+    elif comando.startswith("scp "):
+        clientSocket.send(comando.encode("UTF-8"))
+        modifiedMessage = clientSocket.recv(2048)
+        if modifiedMessage.decode("UTF-8") == "null":
+            print("Arquivo não encontrado.")
+        else:
+            # Recebe o nome/caminho do arquivo
+            caminho = modifiedMessage
+
+            # os.path.join extrai o nome do arquivo a partir do caminho completo
+            nome_arq = os.path.basename(caminho.decode("UTF-8"))
+            print("Nome do arquivo: " + nome_arq)
+
+            # Recebe o tamanho do arquivo
+            modifiedMessage = clientSocket.recv(2048)
+            tam_arquivo = modifiedMessage.decode("UTF-8")
+            tam_arquivo = int(tam_arquivo)  # Convertendo de string para inteiro
+            print("Tamanho do arquivo: ", tam_arquivo)
+
+            with open(nome_arq, 'wb') as file:
+                while tam_arquivo > 0:
+                    tam_buffer = 1024 if tam_arquivo >= 1024 else tam_arquivo
+
+                    # Busca os dados do arquivo
+                    dados = clientSocket.recv(tam_buffer)
+
+                    # Escreve no arquivo
+                    file.write(dados)
+                    tam_arquivo -= len(dados)
+
+                    # Envia a confirmação de recebimento ao servidor
+                    clientSocket.send("ACK".encode("UTF-8"))
+            print(f"Arquivo '{nome_arq}' baixado.")
     else:
         break
-
 clientSocket.close()
